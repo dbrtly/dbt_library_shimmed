2150703a17ab3e49d5072bb03378d9e492c27cd1
Daniel Bartley <dbrtly00@.gmail.com>
2023-12-15 18:56:08 +1100

    still not working

--- dbt_libraries/dbt_library_shims/macros/my_macro.sql
+++ dbt_libraries/dbt_library_shims/macros/my_macro.sql
@@ -1,3 +1,7 @@
+{%- macro my_macro() -%}
+'dbt_library_shimmed'
+{%- endmacro %}
+
 {%- macro default__my_macro() -%}
 'dbt_library_shimmed'
 {%- endmacro %}
--- macros/my_macro.sql
+++ macros/my_macro.sql
@@ -1,9 +1,13 @@
+{%- macro my_macro() -%}
+    {{ return(adapter.dispatch('my_macro', 'dbt_unit_testing')()) }}
+{%- endmacro %}
+
 {%- macro default__my_macro() -%}
 'dbt_project_shimmed'
 {%- endmacro %}
 
 {%- macro bigquery__my_macro() -%}
-'dbt_project_shimmed'
+'bugquery dbt_project_shimmed'
 {%- endmacro %}
 
 {%- macro duckdb__my_macro() -%}
